078e0ee697eefb27ce1490ae5406c5245d023acd
Ian D. McCunn <imccunn@expedia.com>
2017-02-21 20:03:56 -0800

    Make question 7 runnable from CLI; cleanup.

--- Question_7/findPrimeFactors.js
+++ Question_7/findPrimeFactors.js
@@ -1,12 +1,16 @@
 
+let n = process.argv[2];
+
+console.log(`Prime factors of ${n} are ${findPrimeFactors(n)}.`);
+
 function findPrimeFactors(n) {
-  if ( n < 0 ) {
-     return false;
+  if (n < 0) {
+     return [];
   } else {
     var factorsOfNum = factors(n);
     var primeFactorsOfNum = [];
-    for (var i = 0; i < factorsOfNum.length; i++) {
-      var curr = factorsOfNum[i];
+    for (let i = 0; i < factorsOfNum.length; i++) {
+      let curr = factorsOfNum[i];
       if (isPrime(curr)) primeFactorsOfNum.push(curr);
     }
     return primeFactorsOfNum;
